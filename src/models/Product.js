c3bedc1c4d464b64da5fac8bb9f9dc75f0878c59
M1CH4EL-ux2 <joaomichael912@gmail.com>
2022-01-06 17:29:22 -0300

    Criação de produto feita

--- src/models/Product.js
+++ src/models/Product.js
@@ -21,7 +21,8 @@ const Schema = new mongoose.Schema({
     },
     username: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+        ref: "User",
+        required: true
     }
 });
 
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const SessionController = require('../controllers/Login')
+const ProductController = require('../controllers/ProductController')
 const UserController = require('../controllers/UserController')
 
 const routes = Router()
@@ -16,13 +17,13 @@ routes.get('/users/:user_id', UserController.getUserById)
 
 routes.post('/sessions', SessionController.createSession)
 
-routes.post('/products/:user_id')
-routes.get('/products/:user_id')
-routes.patch('/products/:user_id/product_id')
-routes.delete('/products/:user_id/product_id')
+routes.post('/products/:user_id', ProductController.createProduct)
+routes.get('/products/:user_id', ProductController.getUserProducts)
+routes.patch('/products/:user_id/product_id', ProductController.updateProducts)
+routes.delete('/products/:user_id/product_id', ProductController.deleteProduct)
 
-routes.get('/products')
-routes.get('/products/:product_id')
+routes.get('/products', ProductController.getProducts)
+routes.get('/products/:product_id', ProductController.getProductsById)
 
 routes.post('/cart/:user_id')
 routes.get('/cart/:user_id')
