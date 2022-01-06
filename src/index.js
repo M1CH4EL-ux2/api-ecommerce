0afadbac66d13a7b12f6113fb0eef4fd904e5932
M1CH4EL-ux2 <joaomichael912@gmail.com>
2022-01-06 00:23:43 -0300

    Criação do arquivo de rotas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Ola mundo!")
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./routes/router')
+
+require('dotenv').config()
+
+const server = express()
+
+
+mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}, console.log("conected to database"))
+
+server.use(cors())
+server.use(express.json())
+server.use(routes)
+
+server.listen(8080, () => {
+    console.log("rodando na porta http://localhost:8080")
+})
