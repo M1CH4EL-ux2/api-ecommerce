aeecb70511b06f7a461ae7f46284c8de94e665f1
M1CH4EL-ux2 <joaomichael912@gmail.com>
2022-01-09 21:02:59 -0300

    Rota de listagem de produtos feita

--- src/controllers/ProductController/index.js
+++ src/controllers/ProductController/index.js
@@ -55,7 +55,9 @@ const ProductController = {
     },
     async getProducts(req, res) {
         try {
-            
+            const products = await Product.find();
+
+            return res.status(200).json(products)
         } catch (err) {
             return res.status(400).json(err)
         }
